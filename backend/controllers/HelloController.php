602de0539f0953486ab839e43e26703d899720c0
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-05-18 12:19:28 +0100

    First BO Controller: http://backend.test/hello/index

---
+++ backend/controllers/HelloController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\web\Response;
+
+/**
+ * Hello controller
+ */
+
+class HelloController extends Controller
+{
+
+    public function actionIndex(){
+        return "HelloWorld";
+    }
+
+}
